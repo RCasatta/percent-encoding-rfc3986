99c8d3e62087468e100868b235a27dd3f91ec0cc
Riccardo Casatta <riccardo@casatta.it>
2021-09-20 12:50:22 +0200

    use rt

---
+++ fuzz/fuzz_targets/rt.rs
@@ -0,0 +1,12 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+use percent_encoding_rfc3986::{utf8_percent_encode, percent_decode_str, NON_ALPHANUMERIC};
+
+// cargo +nightly fuzz run encode -- -only_ascii=1
+
+fuzz_target!(|data: &[u8]| {
+    let original = std::str::from_utf8(data).unwrap();
+    let encoded = utf8_percent_encode(original, NON_ALPHANUMERIC).to_string();
+    let decoded = percent_decode_str(&encoded).unwrap().decode_utf8().unwrap();
+    assert_eq!(original, decoded);
+});
